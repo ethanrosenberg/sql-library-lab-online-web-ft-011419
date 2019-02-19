12fdeffa7cb5e1eb736507173a63269da87b2731
Ethan Rosenberg <ethan@ethanrosenberg.com>
2019-02-19 05:36:38 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,11 @@
+
+
+
+INSERT INTO characters (name, species, motto, series_id, author_id) VALUES ('Sherlock Holmes', "human", "The game is a foot!", 1, 1);
+INSERT INTO characters (name, species, motto, series_id, author_id) VALUES ('Dr. Watson', "human", "Holmes lookout!", 1, 1);
+INSERT INTO characters (name, species, motto, series_id, author_id) VALUES ('Sherlock Holmes', "human", "The game is a foot!", 1, 1);
+INSERT INTO characters (name, species, motto, series_id, author_id) VALUES ('Sherlock Holmes', "human", "The game is a foot!", 1, 1);
+INSERT INTO characters (name, species, motto, series_id, author_id) VALUES ('Sherlock Holmes', "human", "The game is a foot!", 1, 1);
+INSERT INTO characters (name, species, motto, series_id, author_id) VALUES ('Sherlock Holmes', "human", "The game is a foot!", 1, 1);
+INSERT INTO characters (name, species, motto, series_id, author_id) VALUES ('Sherlock Holmes', "human", "The game is a foot!", 1, 1);
+INSERT INTO characters (name, species, motto, series_id, author_id) VALUES ('Sherlock Holmes', "human", "The game is a foot!", 1, 1);
