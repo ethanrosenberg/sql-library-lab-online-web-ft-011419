605cc1d91cb0d27d3d182044f0a6326f7395697d
Ethan Rosenberg <ethan@ethanrosenberg.com>
2019-02-19 05:23:44 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -32,3 +32,12 @@ CREATE TABLE characters (
   series_id INTEGER,
   author_id INTEGER
 );
+
+CREATE TABLE character_books (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  species TEXT,
+  motto TEXT,
+  series_id INTEGER,
+  author_id INTEGER
+);
