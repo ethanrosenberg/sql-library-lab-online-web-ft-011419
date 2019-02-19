a1b7d7b57925cf6c2a03eea9467d73264801e3cb
Ethan Rosenberg <ethan@ethanrosenberg.com>
2019-02-19 05:41:44 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -10,8 +10,8 @@ INSERT INTO characters (name, species, motto, series_id, author_id) VALUES ('She
 INSERT INTO characters (name, species, motto, series_id, author_id) VALUES ('Sherlock Holmes', "human", "The game is a foot!", 1, 1);
 INSERT INTO characters (name, species, motto, series_id, author_id) VALUES ('Sherlock Holmes', "human", "The game is a foot!", 1, 1);
 
-INSERT INTO series (id, title, author_id, subgenre_id) VALUES ("The Original Sherlock Holmes Volume 1", 1, 1);
-INSERT INTO series (id, title, author_id, subgenre_id) VALUES ("The Return of Sherlock Holmes", 1, 1);
+INSERT INTO series (title, author_id, subgenre_id) VALUES ("The Original Sherlock Holmes Volume 1", 1, 1);
+INSERT INTO series (title, author_id, subgenre_id) VALUES ("The Return of Sherlock Holmes", 1, 1);
 
 INSERT INTO subgenres (name) VALUES ("Mystery Thriller");
 INSERT INTO subgenres (name) VALUES ("Adventure Thriller");
