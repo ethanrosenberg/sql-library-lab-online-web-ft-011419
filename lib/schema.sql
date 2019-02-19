bc26a936aa6e781bc61630b60d2b23066750dcc2
Ethan Rosenberg <ethan@ethanrosenberg.com>
2019-02-19 05:18:25 +0000

    Automatically backed up by Learn

---
+++ lib/schema.sql
@@ -0,0 +1,32 @@
+CREATE TABLE series (
+id INTEGER PRIMARY KEY,
+title TEXT,
+author_id INTEGER,
+subgenre_id INTEGER
+  
+);
+
+CREATE TABLE subgenres (
+
+  
+);
+
+CREATE TABLE authors (
+
+  
+);
+
+CREATE TABLE books (
+
+  
+);
+
+CREATE TABLE characters (
+
+  
+);
+
+CREATE TABLE character_books (
+
+  
+);
