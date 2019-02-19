dd3e7613a8f9da477cdd6751ac34c221a57696e2
Ethan Rosenberg <ethan@ethanrosenberg.com>
2019-02-19 06:01:29 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -5,10 +5,16 @@ INSERT INTO characters (name, species, motto, series_id, author_id) VALUES ('She
 INSERT INTO characters (name, species, motto, series_id, author_id) VALUES ('Dr. Watson', "human", "Holmes lookout!", 1, 1);
 INSERT INTO characters (name, species, motto, series_id, author_id) VALUES ('Ms Moneypenny', "human", "Mr Holmes!!!", 1, 1);
 INSERT INTO characters (name, species, motto, series_id, author_id) VALUES ('Percy', "human", "Right you are...", 1, 1);
-INSERT INTO characters (name, species, motto, series_id, author_id) VALUES ('Dr. Moriarity', "human", "You will die Holmes", 1, 1);
-INSERT INTO characters (name, species, motto, series_id, author_id) VALUES ('Dan Cosgrove', "human", "Looks great chap", 1, 1);
-INSERT INTO characters (name, species, motto, series_id, author_id) VALUES ('Milton Smith', "human", "Indeed. I concur", 1, 1);
-INSERT INTO characters (name, species, motto, series_id, author_id) VALUES ('John Dallinger', "human", "I have to respectfully decline", 1, 1);
+
+INSERT INTO characters (name, species, motto, series_id, author_id) VALUES ('Dr. Moriarity', "human", "You will die Holmes", 2, 1);
+INSERT INTO characters (name, species, motto, series_id, author_id) VALUES ('Dan Cosgrove', "human", "Looks great chap", 2, 1);
+INSERT INTO characters (name, species, motto, series_id, author_id) VALUES ('Milton Smith', "human", "Indeed. I concur", 2, 1);
+INSERT INTO characters (name, species, motto, series_id, author_id) VALUES ('John Dallinger', "human", "I have to respectfully decline", 2, 1);
+
+
+INSERT INTO character_books (book_id, character_id) VALUES (1, 1);
+INSERT INTO character_books (book_id, character_id) VALUES (2, 1);
+
 
 INSERT INTO series (title, author_id, subgenre_id) VALUES ("The Original Sherlock Holmes Volume 1", 1, 1);
 INSERT INTO series (title, author_id, subgenre_id) VALUES ("The Return of Sherlock Holmes", 1, 2;
